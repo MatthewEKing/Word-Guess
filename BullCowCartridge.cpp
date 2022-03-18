da9b57972a54ccaaec624bf91f341e5ce346833c
Matt <matt.king1119@gmail.com>
2022-03-18 15:36:46 -0400

    Add files via upload

--- BullCowCartridge.cpp
+++ BullCowCartridge.cpp
@@ -1,13 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "WordList.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
     SetupGame();
-
+    
+    PrintLine(TEXT("Number of Words: %i"), Words.Num());
     PrintLine(TEXT("The Hidden Word is: %s. \nIt is %i letters long"), *HiddenWord, HiddenWord.Len()); // Debug Line
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
@@ -26,7 +29,7 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 void UBullCowCartridge::SetupGame()
 {
     //Initialize Variables
-    HiddenWord = TEXT("test");
+    HiddenWord = TEXT("king");
     Lives = HiddenWord.Len();
     bGameOver = false;
 
@@ -51,16 +54,17 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
         return;
     }
 
-    //if (!IsIsogram)
-    //{
-    //    //check if isogram
-    //    PrintLine(TEXT()"No Repeating Letters, guess again")
-    //}
-
     if (Guess.Len() != HiddenWord.Len())
     {
         PrintLine(TEXT("Try Again, \nYou have %i lives left"), Lives);
         PrintLine(TEXT("The hidden word is %i letters long"), HiddenWord.Len());
+        return;
+    }
+
+    if (!IsIsogram(Guess))
+    {
+        //check if isogram
+        PrintLine(TEXT("No Repeating Letters, guess again"));
         return;
     }
 
@@ -76,6 +80,21 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
     }
 
     PrintLine(TEXT("Guess Again, you have %i lives left"), Lives);
-    //Check Isogram
-    //Check Number of Characters (position and if they are right or wrong)
+}
+
+bool UBullCowCartridge::IsIsogram(FString Word) const
+{
+    for (int32 Index = 0; Index < Word.Len(); Index++)
+    {
+        for (int32 Comparison = Index + 1; Comparison < Word.Len(); Comparison++)
+        {
+            if (Word[Index] == Word[Comparison])
+            {
+                //PrintLine(TEXT("%c, %c"), Word[Index], Word[Comparison]); //Check Repeating Character
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
---
+++ WordList.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+const TArray<FString> Words = 
+{ 
+	TEXT("king"),
+	TEXT("industry"),
+	TEXT("stickbug"),
+	TEXT("ye"),
+	TEXT("isogram")
+};
